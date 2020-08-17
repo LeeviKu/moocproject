762a5ee157fa2daf3263bd2240d1af4a0a176c57
LeeviKu <leevi.kukkula@gmail.com>
2020-08-18 02:39:35 +0300

    fixes

---
+++ src/main/java/fi/leevi/project/DefaultController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.leevi.project;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author noob9s
+ */
+
+@Controller
+public class DefaultController {
+    
+    @ResponseBody
+    @GetMapping("/")
+    public String home() {
+        return "asd";
+    }
+}
