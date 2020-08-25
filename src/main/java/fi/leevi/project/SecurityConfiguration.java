2816ab33f224ae34f38eed152d53e89e9cb5dbec
LeeviKu <leevi.kukkula@gmail.com>
2020-08-25 12:29:41 +0300

    login and signup

---
+++ src/main/java/fi/leevi/project/SecurityConfiguration.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.leevi.project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ *
+ * @author Lepe
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+     
+    @Autowired
+    private UserDetailsService userDetailsService;
+ 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // poistetaan csrf-tarkistus käytöstä h2-konsolin vuoksi
+        http.csrf().disable();
+        // sallitaan framejen käyttö
+        http.headers().frameOptions().sameOrigin();
+
+        http.authorizeRequests()
+                .antMatchers("/h2-console", "/h2-console/**").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/frontpage").permitAll()
+                .antMatchers("/signup").permitAll()
+                .anyRequest().authenticated();
+        http.formLogin().loginPage("/login")
+                .permitAll();
+                
+    }
+ 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+ 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
