a5e73495ff66920f0472f6e6b85d2fa7c46c61e8
LeeviKu <leevi.kukkula@gmail.com>
2020-09-08 20:03:51 +0300

    add profile.html for profile pages and method profile() in class UserController for handling get requests for profile pages

--- src/main/java/fi/leevi/project/UserController.java
+++ src/main/java/fi/leevi/project/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -41,6 +42,12 @@ public class UserController {
         User user = new User(username, passwordEncoder.encode(password), name, path);
         userRepository.save(user);
         return "redirect:/login";
+    }
+    
+    @GetMapping("profile/{path}")
+    public String profile(@PathVariable String path, Model model) {
+        model.addAttribute("person", userRepository.findByPath(path));
+        return "profile";
     }
 
 }
--- src/main/java/fi/leevi/project/UserRepository.java
+++ src/main/java/fi/leevi/project/UserRepository.java
@@ -13,5 +13,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+    User findByPath(String path);
 }
 
