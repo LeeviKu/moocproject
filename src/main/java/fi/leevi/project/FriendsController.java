b874f020024f4bdb98b313002490808dc93b942e
LeeviKu <leevi.kukkula@gmail.com>
2020-09-10 14:10:27 +0300

    Change add friend form in friends.html to have link to /profile/add/{name}

--- src/main/java/fi/leevi/project/FriendsController.java
+++ src/main/java/fi/leevi/project/FriendsController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -32,14 +33,13 @@ public class FriendsController {
         System.out.println(name);
         if (userRepository.findByName(name) != null) {
             model.addAttribute("searchresult", userRepository.findByName(name));
-            System.out.print(userRepository.findByName(name).getName());
         }
         return "friends";
     }
     
-    @PostMapping("/friends/add")
-    public String addFriend() {
-        System.out.println("added");
+    @PostMapping("/friends/add/{name}")
+    public String addFriend(@PathVariable String name) {
+        System.out.println(name);
         return "redirect:/friends";
     }
 }
